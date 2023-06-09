0538f4c463f15b49ac4fb9dc0fef40e33053f251
PankajMehta <pankaj.mehta@ebizondigital.com>
2023-06-09 13:56:09 +0530

    vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  data() {
+    return (
+      {
+        name: 'pankaj'
+      }
+    )
+  },
+  props:
+  {
+    msg: Number
+  },
+  methods:
+  {
+    hello() {
+      console.log("hi pankaj");
+    },
+    change(e)
+    {
+      console.log(e.target.value)
+    }
+
+  }
+  
+};
+</script>
+
+<template>
+  <div class="greetings">
+    <p>{{ name }}</p>
+    <p>{{ msg }}</p>
+    <button v-on:click="hello">Click me</button>
+    <input v-model="text" v-on:change="change"/>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
